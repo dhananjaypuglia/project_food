670291515c4da89981d272e8b261a0de9a60880f
Dhananjay Puglia <dhansiddh@gmail.com>
2014-12-31 12:09:22 +0530

    changed structure

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ProjectFood::Application.config.secret_token = '4d7a9835b9b31cae83ddce3f1ed3cf89d68dee22534061eae50c777e6ca537bfeaf9ee3ab96ea868f848cb297c609819eabb26b995dca4a70ef1687fb5641243'
